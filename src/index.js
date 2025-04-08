f4849e7522fd8f347ec883f0b28f31b528245cf8
hhoot25 <hhoot25@gmail.com>
2025-04-07 22:21:52 -0500

    c+v other tabs and added event listener

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import "./styles.css";
 
+
+
 import {homeTab} from './home.js';
+import {menuTab} from './menu.js';
+import {aboutTab} from './about.js';
+
+
 console.log("this is the index.js message");
 
+const home = document.querySelector('#home');
+const menu = document.querySelector('#menu');
+const about = document.querySelector('#about');
+
+const content = document.querySelector("#content");
+
+home.addEventListener("click",()=>{
+    content.innerHTML = "";
+    homeTab();
+
+});
+
+menu.addEventListener("click",()=>{
+    content.innerHTML = "";
+    menuTab();
+});
+
+
+about.addEventListener("click",()=>{
+    content.innerHTML = "";
+    aboutTab();
+});
 
 
 
---
+++ src/menu.js
@@ -0,0 +1,43 @@
+
+import strawberryMatcha from "./images/strawberryMatcha.png";
+
+
+
+export function menuTab(){
+    const content = document.querySelector('#content');
+
+    //content title
+    const top = document.createElement("div");
+    const newContent = document.createTextNode("This is the menu tab!");
+    top.classList.add('top');
+
+    top.appendChild(newContent);
+
+    content.appendChild(top);
+
+    //add matcha photo
+    const image = document.createElement('img');
+    image.src = strawberryMatcha;
+    content.appendChild(image);
+
+    //mission statement
+    const box1 = document.createElement('div');
+    box1.classList.add('card');
+
+    const box1title = document.createElement('div');
+    box1title.classList.add('title');
+ 
+    const quoteTitle = document.createTextNode("Mission Statement");
+    const statementText = document.createTextNode("\"It all started with the BOGO sign on the door, buy one Strawberry Matcha get one! That when the idea struck me. Why isn't there a matcha store?\"");
+    const creditText = document.createTextNode(" -Matt Endeavor");
+
+    box1title.appendChild(quoteTitle);
+
+    box1.appendChild(box1title);
+    box1.appendChild(statementText);
+    box1.appendChild(creditText);
+
+    content.appendChild(box1);
+    
+
+}
